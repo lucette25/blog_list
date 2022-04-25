fe3eb7c93dee4b69eae6f9b27702d62359cfe10e
lucette25 <hermione.odjo@imsp-uac.org>
2022-04-25 21:15:45 +0300

    blogslist_tests_step5

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,46 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
 const app = require('../app')
 const api = supertest(app)
-const Blog = require('../models/blog')
 
 
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5
-  },
-  {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12
-  },
-  {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
-    likes: 10
-  }
-]
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[2])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[3])
+  blogObject = new Blog(helper.initialBlogs[2])
   await blogObject.save()
 })
 
@@ -52,6 +26,11 @@ test('blogs are returned as json', async () => {
 }, 100000)
 
 
+test('there are three blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(3)
+}, 100000)
 
 test('the unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
@@ -59,8 +38,80 @@ test('the unique identifier property of the blog posts is named id', async () =>
 
 }, 100000)
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+    likes: 10
+  }
+
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(b => b.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain(
+    'First class tests'
+  )
+})
+
+test('blog without likes can be added', async () => {
+  const newBlog = {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+  }
+
+  if(  newBlog.likes===undefined)
+    newBlog.likes=0
+
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(b => b.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain(
+    'First class tests'
+  )
+})
+
+test('blog without title and url is not added', async () => {
+  const newBlog = {
+    author: 'Robert C. Martin'
+  }
+  console.log(newBlog.title)
+  if(  newBlog.title===undefined || newBlog.url===undefined){
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  }
+
+  const response = await api.get('/api/blogs')
+
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+
+})
 
 
 afterAll(() => {
   mongoose.connection.close()
 })
+
+//npm test -- tests/blog_api.test.js
